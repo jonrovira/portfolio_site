a466a49c2677949e98e78390032a95f87d3b18c3
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-09-07 23:33:33 -0500

    finishd simple portfolio gallery, adding item info below images now

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,69 +6,46 @@ $(document).ready(function() {
 
 	// Setup the portfolio
 	//   * Set width of items wrapper
-	//   * Set first portfolio item as active item
 	var setupPortfolio = function() {
 
 		// Set width of portfolio items wrapper for slider to stay on one line
 		var totalWidth = 0;
-		$('section#portfolio div.section-content ul#portfolio-items-wrapper li').each(function() {
+		$('section#portfolio div.section-content div#portfolio-items-wrapper ul#portfolio-gallery li').each(function() {
 			totalWidth += $(this).outerWidth(true);
 		});
-		$('section#portfolio div.section-content ul#portfolio-items-wrapper').width(totalWidth + 29);
+		$('section#portfolio div.section-content div#portfolio-items-wrapper ul#portfolio-gallery').width(totalWidth + 29);
 
-		// Set first portfolio item in list as active
-		$('section#portfolio div.section-content ul#portfolio-items-wrapper li:first-child').addClass('active');
-
-		// Blur items outside visible scope of portfolio list
 		blurPortfolioItems();
 	};
 	// Blur items outside visible scope
 	var blurPortfolioItems = function() {
-		// Blur items outside visible scope of portfolio list
-		// var leftMargin = parseFloat($('section#portfolio div.section-content').css('margin-left').replace('px', ''));
-		// var visibleWidth = $('section#portfolio div.section-content').width() - leftMargin;
-		// var currentPos = 0;
-		var current = $('section#portfolio div.section-content ul#portfolio-items-wrapper li.active');
-		current.removeClass('portfolio-item-blurred');
-		current.siblings().each(function() {
-			
-		});
-		$('section#portfolio div.section-content ul#portfolio-items-wrapper li').each(function() {
-			if( $(this) )
-			currentPos += $(this).outerWidth(true);
-			if( currentPos > visibleWidth ) {
-				$(this).addClass('portfolio-item-blurred');
+		
+		// Get dimensions of visilble region
+		var leftMargin = parseFloat($('section#portfolio div.section-content').css('margin-left').replace('px', ''));
+		var visibleWidth = $('section#portfolio div.section-content').width() - leftMargin;
+		var itemWidth = $('section#portfolio div.section-content div#portfolio-items-wrapper ul#portfolio-gallery li.portfolio-item a img').width();
+
+		// Get coordinates of visible region
+		var leftBoundary = $('section#portfolio div.section-content div#portfolio-items-wrapper').scrollLeft();
+		var rightBoundary = leftBoundary + visibleWidth;
+
+		// Determine which should be blurred, blur those
+		$('section#portfolio div.section-content div#portfolio-items-wrapper ul#portfolio-gallery li').each(function() {
+			var rightPosition = $(this).position().left + itemWidth;
+			if( rightPosition <= rightBoundary ) {
+				if( $(this).hasClass('portfolio-item-blurred') ) {
+					$(this).removeClass('portfolio-item-blurred');
+					$(this).fadeTo("fast", 1.0);
+				}
 			}
 			else {
-				$(this).removeClass('portfolio-item-blurred');
+				if( !$(this).hasClass('portfolio-item-blurred') ) {
+					$(this).addClass('portfolio-item-blurred');
+					$(this).fadeTo("fast", 0.5);
+				}
 			}
 		});
 	}
-	// Animate portfolio gallery by one element in the correct direction
-	var toPortfolioItem = function(direction) {
-
-		// Find target element based on passed direction
-		var $target;
-		if( direction == 'left' ) {
-			$target = $('section#portfolio div.section-content ul#portfolio-items-wrapper li.active').prev();
-		}
-		else if( direction == 'right' ) {
-			$target = $('section#portfolio div.section-content ul#portfolio-items-wrapper li.active').next();	
-		}
-
-		// Get position of new target element
-		var newPosition = $target.position().left;
-		$target.addClass('active');
-		$target.siblings().removeClass('active');
-
-		// Animate list to that element
-		$('section#portfolio div.section-content ul#portfolio-items-wrapper').animate({
-			left : - newPosition
-		});
-
-		// Blur items outside visible scope of portfolio list
-		blurPortfolioItems();
-	};
 
 
 
@@ -84,15 +61,8 @@ $(document).ready(function() {
 	 * Events
 	 */
 
-	// Next button in portfolio gallery is clicked -> animate portfolio gallery to next item if possible
-	$('section#portfolio div.section-content i#portfolio-prev').click(function() {
-		// placeholder for minimize
-		toPortfolioItem('left');
-	});
-	// Previous button in portfolio gallery is clicked -> animate portfolio gallery to previous item if possible
-	$('section#portfolio div.section-content i#portfolio-next').click(function() {
-		// placeholder for minimize
-		toPortfolioItem('right');
+	$('section#portfolio div.section-content div#portfolio-items-wrapper').scroll(function() {
+		blurPortfolioItems();
 	});
 
 });
